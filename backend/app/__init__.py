69e3a7b5a4ef1f9e1ff85f1e2074f9f0db2326fa
My Nguyen <chiffonng136@gmail.com>
2024-01-28 21:46:05 +0700

    add layout for tasks and subtasks

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from .config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+from app.views import routes
